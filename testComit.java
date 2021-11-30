0b680ca0bc5e428f42927680f416356f92e2138a
DESKTOP-I0KOF80\Пользователь <admin54@admkor.ru>
2021-11-30 21:07:46 +0300

    test time

--- testComit.java
+++ testComit.java
@@ -1,2 +1,8 @@
-package CSV;public class testComit {
+package CSV;
+
+public class testComit {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+    String test = "Time";
 }
